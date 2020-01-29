863c83639e0a2dc90d5e12a5b7451394ab7ad8a6
myexistance <joonyoonlives@gmail.com>
2020-01-30 00:15:50 +1300

    initial with header

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <nav>
+    <h2>React Boost </h2>
+    <ul>
+    <li>Home</li>
+    <li>Release</li>
+    <li>Contact</li>
+    </ul>
+
+    </nav>
+  );
+}
+
+export default Nav;
