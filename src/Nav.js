2ae9f3c294c03efde9f13e89786e8c3556b274a2
myexistance <joonyoonlives@gmail.com>
2020-01-30 01:52:04 +1300

    Bootstrap and MDB React pro Nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,20 +1,75 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import './Nav.css';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBContainer }
+from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function Nav() {
-  return (
-    <nav>
-    <h2>React Boost </h2>
-    <ul>
-    <li>Home</li>
-    <li>Release</li>
-    <li>Contact</li>
-    </ul>
+// function Nav() {
+//   return (
+//     <nav>
+//     <h2>React Boost </h2>
+//     <ul>
+//     <li href="">Home</li>
+//     <li>Release</li>
+//     <li>Contact</li>
+//     <li>Contact</li>
+//     <li>Contact</li>
+//     <li>Contact</li>
+//     </ul>
+//
+//     </nav>
+//   );
+// }
+class Nav extends Component {
+  state = {
+    collapseID: ''
+  };
 
-    </nav>
-  );
+  toggleCollapse = collapseID => () => {
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ''
+    }));
+  };
+
+  render() {
+    return (
+      <Router>
+        <MDBContainer>
+          <MDBNavbar
+            color='light-blue lighten-4'
+            style={{ marginTop: '20px' }}
+            light
+          >
+            <MDBContainer>
+              <MDBNavbarBrand>Yeezy Boost</MDBNavbarBrand>
+              <MDBNavbarToggler
+                onClick={this.toggleCollapse('navbarCollapse1')}
+              />
+              <MDBCollapse
+                id='navbarCollapse1'
+                isOpen={this.state.collapseID}
+                navbar
+              >
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to='#!'>Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to='#!'>Link</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to='#!'>Contact</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+        </MDBContainer>
+      </Router>
+    );
+  }
 }
 
 export default Nav;
